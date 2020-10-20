9e1b53e0f5f496ad388d78e1daefd520b35a9d16
vproskurin <vladimir.proskurin@gmail.com>
2020-10-19 23:33:31 -0700

    basic auth

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const promisify = require("util"); // for wrapping up googleapi callbacks
+const { google } = require("googleapis");
+
+const auth = new google.auth.GoogleAuth({
+    keyFile: "./client_secret.json",
+    scopes: ['https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/documents'],
+})
 
 // 1 Create an express app for serving the React based form and for API for the form and for google API connections
 
 // 2 connect to Drive, Doc APIs as a service account
 
-// 3 for "/" route send a res as a form page 
+// 3 for "/" route send a res as a form page
 
 // 4 when the form is complete and Merge button is pushed send an API call to express endpoint
-//  with form data that will go to google API. 
+//  with form data that will go to google API.
 
 // 5 This is tricky  - we need to connect as this user to his drive so we need to copy the template document into his drive
 //   using user's credentials to give the user full control
-
 
 // Managing templates https://developers.google.com/docs/api/how-tos/merge#managing_templates
 // For template documents that the application defines and owns, create the template using a dedicated account representing the application. Service accounts are a good choice and avoid complications with G Suite policies that restrict sharing.
